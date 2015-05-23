6d03da58f0dad6be83d2f3d50bb2615298bf769d
dmeowmixer <dmeowmixer@hotmail.com>
2015-05-23 03:38:13 -1000

    THOSE FACEBOOK DOCS

--- fr/plugins/facebook.js
+++ fr/plugins/facebook.js
@@ -5,8 +5,8 @@ var FB = require('fb');
 
 
 FB.api('oauth/access_token', {
-    client_id: 905091946220401,
-    client_secret: '57726255633ffd7500f85ce8717e1635',
+    client_id: 905205639542365,
+    client_secret: 'acdb29d7fda9bc3bb96ef4f1ab679de1',
     grant_type: 'client_credentials'
 }, function (res) {
     if(!res || res.error) {
