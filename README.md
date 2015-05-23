03e9c923cf89b71bcc95794cb38463ba5f728a8a
Edward Kim <ed@slickage.com>
2015-05-23 07:08:42 -1000

    link to python mongo goodness

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # slugwithfriends
 Slugging with your buddy.
+
+###Requirements:###
+ * Python 2.7.6  
+ * pip  
+ * node  
+ * MongoDB
+
+
+##Sets up the MongoDB##
+pip install -r modals/requirements.txt
+
+
+## Running MongoDB##
+python modals/run.py
+
+
+## Starting Frontend Server ##
+node fr/server
--- fr/routes/index.js
+++ fr/routes/index.js
@@ -1,6 +1,8 @@
 /* jshint node: true */
 'use strict';
 var path = require('path');
+var Promise = require('bluebird');
+var req = Promise.promisifyAll(require('request'));
 var fb = require(path.normalize(__dirname + '/../plugins/facebook'));
 
 module.exports = [
@@ -20,7 +22,6 @@ module.exports = [
     handler: { file: 'index.html' }
   },
   {
-    //
     method: 'POST',
     path: '/api/saveLogin',
     config: {
@@ -30,5 +31,36 @@ module.exports = [
         reply();
       }
     }
+  },
+  {
+    method: 'GET',
+    path: '/api/slugs',
+    config: {
+      handler: function(request, reply) {
+        req.getAsync('http://localhost:5000/slugs')
+        .then(function(users) { return processRows(users); })
+        .then(reply);
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/api/locations',
+    config: {
+      handler: function(request, reply) {
+        req.getAsync('http://localhost:5000/locations')
+        .then(function(users) { return processRows(users); })
+        .then(reply);
+      }
+    }
   }
 ];
+
+
+function processRows(rows) {
+  rows.shift();
+  rows = rows.map(function(row) {
+    return JSON.parse(row)._items;
+  });
+  return rows;
+}
