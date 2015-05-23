a28ca932e9be6385895aed07a85022c935b34f2a
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-05-23 02:51:32 -1000

    much page.  wow.

--- fr/public/js/app.js
+++ fr/public/js/app.js
@@ -6,7 +6,9 @@ angular.module('SlugApp', [
 ])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider
-  .when("/", {templateUrl: "/static/partials/login.html", controller: "slugController"})
-//  when("/drivers/:id", {templateUrl: "partials/driver.html", controller: "driverController"}).
-  .otherwise({redirectTo: '/'});
+  .when("/login", {templateUrl: "/static/partials/login.html", controller: "slugController"})
+  .when("/home", {templateUrl: "/static/partials/mainmenu.html", controller: "slugController"})
+  .when("/offer", {templateUrl: "/static/partials/offering.html", controller: "slugController"})
+  .when("/search", {templateUrl: "/static/partials/searching.html", controller: "slugController"})
+  .otherwise({redirectTo: '/login'});
 }]);
